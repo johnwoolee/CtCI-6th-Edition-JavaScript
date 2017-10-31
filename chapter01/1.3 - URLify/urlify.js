c8754ed6689b90421da263b3077a02d51c4a9145
John Lee <johnwoolee@gmail.com>
2017-10-31 15:10:00 -0700

    use functional style with map

--- chapter01/1.3 - URLify/urlify.js
+++ chapter01/1.3 - URLify/urlify.js
@@ -1,24 +1,14 @@
 var urlify = function(str, length) {
-  // have a pointer to check from start to end
-  var strArr = str.split('');
-  var pointer = 0;
-  while (pointer < str.length) {
-    if (strArr[pointer] === ' ') {
-      // *** needs more work here, a little wierd
-      // not handling trailing spaces properly
-      for (var i = str.length - 1; i > pointer + 3; i--) {
-        strArr[i] = str[i - 2];
-      }
-      strArr[pointer] = '%';
-      strArr[pointer+1] = '2';
-      strArr[pointer+2] = '0';
-      console.log(strArr, strArr.length);
-    } 
-    pointer++;
-  }
-  // if character is a space, move remainder chars by two
-  // replace following three chars with '%20'
-  return strArr.join('');
+  var subStr = str.substring(0, length);
+  // or use str.trim
+
+	var strArr = subStr.split('');
+  
+  var result = strArr.map((item) => {
+  	return (item === ' ') ? '%20' : item;
+  })
+  
+  return result.join('');
 };
 
 console.log(urlify('Mr John Smith    ', 13), 'Mr%20John%20Smith');
